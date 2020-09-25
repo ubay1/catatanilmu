4ebccb506a71217dff2f78895cb84db33732666d
Aziz Akbar <azis.akbar91@gmail.com>
2020-09-25 11:17:42 +0700

    [add-icon-header]

--- components/header/header.vue
+++ components/header/header.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="header">
     <div class="logo">
-      <img src="icon.png" alt="Catatan Ilmu">
+      <img v-if="isTheme === 'dark'" src="icon.png" alt="Catatan Ilmu">
+      <img v-if="isTheme === 'light'" src="icon2.png" alt="Catatan Ilmu">
     </div>
     <div class="header_title" :class="{dark: isTheme === 'light' ? false : true}">
       Catatan Ilmu
